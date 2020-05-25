bc6ac17e113fd04aa5f325e20cf0812da15bd5eb
jonandernovella <novella.uu@gmail.com>
2020-05-25 12:05:59 +0200

    Improve storage docs
    The storage module docs have been updated with better docstrings.

--- lega/utils/storage.py
+++ lega/utils/storage.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-"""File I/O for disk or S3 Object storage."""
+"""
+File I/O for disk or S3 Object storage.
+"""
 
 import os
 import io
@@ -15,26 +17,58 @@ LOG = logging.getLogger(__name__)
 
 
 class FileStorage():
-    """Storage on disk and related I/O."""
+    """
+    Storage on disk and related I/O.
+    """
 
     def __init__(self, config_section, user):
-        """Initialize backend storage to a POSIX file system."""
+        """
+        Initialize backend storage to a POSIX file system.
+
+        :param config_section: Section in the configuration file
+        :type config_section: str
+        :param user: User of the POSIX file system
+        :type user: str
+        """
         self.prefix = (CONF.get_value(config_section, 'location', raw=True) % user).rstrip('/')
         self.separator = CONF.get_value(config_section, 'separator', raw=True)
 
     def location(self, file_id):
-        """Retrieve file location."""
+        """
+        Retrieve file location.
+
+        :param file_id: Id of the file to locate in the file system
+        :type file_id: str
+        :return: The location of the file
+        :rtype: str
+        """
         name = f"{file_id:0>20}"  # filling with zeros, and 20 characters wide
         name_bits = [name[i:i+3] for i in range(0, len(name), 3)]
         target = Path('/').joinpath(*name_bits)
         return str(target)
 
     def filesize(self, path):
-        """Return the size of the file pointed by ``path``."""
+        """
+        Return the size of the file pointed by ``path``.
+
+        :param path: File path
+        :type path: str
+        :return: File size
+        :rtype: int
+        """
         return os.stat(Path(self.prefix + self.separator + path.lstrip('/'))).st_size
 
     def copy(self, fileobj, location):
-        """Copy file object at a specific location."""
+        """
+        Copy file object at a specific location.
+
+        :param fileobj: File object
+        :type fileobj: file
+        :param location: Target file object location
+        :type location: str
+        :return: File size of file at location
+        :rtype: int
+        """
         target = Path(self.prefix + self.separator + location.lstrip('/'))
         target.parent.mkdir(parents=True, exist_ok=True)
         with open(target, 'wb') as h:
@@ -43,30 +77,66 @@ class FileStorage():
 
     @contextmanager
     def open(self, path, mode='rb'):
-        """Open stored file."""
+        """
+        Open stored file.
+
+        :param path: Path to the file
+        :type path: str
+        :param mode: File opening mode, defaults to 'rb'
+        :type mode: str, optional
+        :yield: The specified file
+        :rtype: file
+        """
         fp = Path(self.prefix + self.separator + path.lstrip('/'))
         f = open(fp, mode)
         yield f
         f.close()
 
     def exists(self, filepath):
-        """Return true if the path exists."""
+        """
+        Return true if the path exists.
+
+        :param filepath: Path to the file
+        :type filepath: str
+        :return: Whether the file path exists or not
+        :rtype: bool
+        """
         fp = Path(self.prefix + self.separator + filepath.lstrip('/'))
         return fp.exists()
 
     def __str__(self):
-        """Return inbox prefix."""
+        """
+        Return inbox prefix.
+
+        :return: The prefix of the inbox
+        :rtype: str
+        """
         return str(self.prefix)
 
 
 class S3FileReader(object):
-    """Implements a few of the BufferedIOBase methods.
+    """
+    Implements a few of the BufferedIOBase methods.
 
     see https://docs.python.org/3/library/io.html#io.BufferedIOBase
     """
 
     def __init__(self, s3, bucket, path, mode='rb', blocksize=1 << 22):  # 1<<22 = 4194304 = 4MB
-        """Initialize class."""
+        """
+        Create a S3 file reader object.
+
+        :param s3: boto S3 client object
+        :type s3: botocore.client.S3
+        :param bucket: S3 bucket
+        :type bucket: str
+        :param path: Path to read within the bucket
+        :type path: str
+        :param mode: File opening mode, defaults to 'rb'
+        :type mode: str, optional
+        :param blocksize: Maximum block size to fetch at a time, defaults to 1<<22
+        :type blocksize: int, optional
+        :raises NotImplementedError: File opening mode not supported
+        """
         if mode != 'rb':  # if mode not in ('rb', 'wb', 'ab'):
             raise NotImplementedError(f"File mode '{mode}' not supported")
         self.mode = mode
@@ -87,11 +157,27 @@ class S3FileReader(object):
         #     self.buffer.write(self.read())
 
     def tell(self):
-        """Return position."""
+        """
+        Return position.
+
+        :return: The position of the file reader
+        :rtype: int
+        """
         return self.loc
 
     def seek(self, loc, whence=0):
-        """Change position to the given byte offset."""
+        """
+        Change position to the given byte offset.
+
+        :param loc: Position of the read/write pointer within the file
+        :type loc: int
+        :param whence: Search relative to, defaults to 0
+        :type whence: int, optional
+        :raises ValueError: Not valid whence
+        :raises ValueError: Position before start of file
+        :return: Current file's position
+        :rtype: int
+        """
         if whence == 0:  # from start
             nloc = loc
         elif whence == 1:  # from here
@@ -106,7 +192,15 @@ class S3FileReader(object):
         return self.loc
 
     def read(self, length=-1):
-        """Read and return up to size bytes."""
+        """
+        Read and return up to size bytes.
+
+        :param length: Length of bytes to read, defaults to -1
+        :type length: int, optional
+        :raises ValueError: The file is closed
+        :return: Specified byte range
+        :rtype: bytes
+        """
         if self.closed:
             raise ValueError('I/O operation on closed file.')
 
@@ -122,52 +216,102 @@ class S3FileReader(object):
         return out
 
     def close(self):
-        """Close object reader."""
+        """
+        Close object reader.
+        """
         if self.closed:
             return
         self.closed = True
 
     def __del__(self):
-        """Prepare for object destruction."""
+        """
+        Prepare for object destruction.
+        """
         self.close()
 
     def __str__(self):
-        """Return string representation."""
+        """
+        Return string representation.
+
+        :return: String representation of S3FileReader
+        :rtype: str
+        """
         return f"<S3FileReader {self.path}>"
 
     __repr__ = __str__
 
     def __enter__(self):
-        """Set things."""
+        """
+        Set things.
+        """
         return self
 
     def __exit__(self, *args):
-        """Prepare for object destruction."""
+        """
+        Prepare for object destruction.
+        """
         self.close()
 
     # Implementations of BufferedIOBase stub methods
 
     def read1(self, length=-1):
-        """Read and return up to size bytes, with at most one call to the underlying raw stream’s read()."""
+        """
+        Read and return up to size bytes, with at most one call to the underlying raw stream’s read().
+
+        :param length: Length of bytes to read,, defaults to -1
+        :type length: int, optional
+        :return: Specified byte range
+        :rtype: bytes
+        """
         return self.read(length)
 
     def detach(self):
-        """Raise unssuported operation."""
+        """
+        Raise unssuported operation.
+
+        :raises io.UnsupportedOperation: Detaching is not supported
+        """
         raise io.UnsupportedOperation()
 
     def readinto(self, b):
-        """Read bytes into a pre-allocated object b and return the number of bytes read."""
+        """
+        Read bytes into a pre-allocated object b and return the number of bytes read.
+
+        :param b: Pre-allocated object
+        :type b: object
+        :return: Number of bytes read
+        :rtype: int
+        """
         datalen = len(b)
         data = self.read(datalen)
         b[:datalen] = data
         return datalen
 
     def readinto1(self, b):
-        """Read bytes into an object."""
+        """
+        Read bytes into an object.
+
+        :param b: Pre-allocated object
+        :type b: object
+        :return: int
+        :rtype: Number of bytes read
+        """
         return self.readinto(b)
 
     def _fetch(self, start, end, max_attempts=10):
-        """Read object from S3."""
+        """
+        Get objects from S3.
+
+        :param start: Start of byte range
+        :type start: int
+        :param end: End of byte range
+        :type end: int
+        :param max_attempts: Number of attempts, defaults to 10
+        :type max_attempts: int, optional
+        :raises RuntimeError: Objects could not be fetched from S3 after all retries
+        :return: The bytes corresponding to a S3 object
+        :rtype: bytes
+        """
         # if end > self.size:
         #     end = self.size
         assert end <= self.size
@@ -193,10 +337,21 @@ class S3FileReader(object):
 
 
 class S3Storage():
-    """S3 object storage and related I/O."""
+    """
+    S3 object storage and related I/O.
+    """
 
     def __init__(self, config_section, bucket, prefix=""):
-        """Initialize S3 object Storage."""
+        """
+        Initialize S3 object Storage.
+
+        :param config_section: Section in the configuration file
+        :type config_section: str
+        :param bucket: S3 bucket
+        :type bucket: str
+        :param prefix: Prefix, defaults to ""
+        :type prefix: str, optional
+        """
         import boto3
         import botocore
         self.endpoint = CONF.get_value(config_section, 's3_url')
@@ -230,16 +385,39 @@ class S3Storage():
         # No need to close anymore?
 
     def location(self, file_id):
-        """Retrieve object location."""
+        """
+        Retrieve object location.
+
+        :param file_id: The id of the file to get the location
+        :type file_id: str
+        :return: Location of the object
+        :rtype: str
+        """
         return str(file_id)
 
     def filesize(self, path):
-        """Return the size of the file pointed by ``path``."""
+        """
+        Return the size of the file pointed by ``path``.
+
+        :param path: Path to the file
+        :type path: str
+        :return: Size of the specified file
+        :rtype: int
+        """
         resp = self.s3.head_object(Bucket=self.bucket, Key=path)
         return resp['ContentLength']
 
     def copy(self, fileobj, location):
-        """Copy file object in a bucket."""
+        """
+        Copy file object in a bucket.
+
+        :param fileobj: File object
+        :type fileobj: file
+        :param location: Target file object location
+        :type location: str
+        :return: File size of file at location
+        :rtype: int
+        """
         from boto3.s3.transfer import TransferConfig
         chunksize = int(CONF.get_value('archive', 's3_chunk_size', default=32 * 1024 * 1024))
         transfer_config = TransferConfig(multipart_threshold=chunksize, max_concurrency=10,
@@ -250,15 +428,36 @@ class S3Storage():
 
     @contextmanager
     def open(self, path, mode='rb'):
-        """Open stored object."""
+        """
+        Open stored object.
+
+        :param path: Path to the file
+        :type path: str
+        :param mode: File opening mode, defaults to 'rb'
+        :type mode: str, optional
+        :yield: The specified file
+        :rtype: S3FileReader
+        """
         f = S3FileReader(self.s3, self.bucket, path, mode=mode)
         yield f
         f.close()
 
     def exists(self, path):
-        """Return true if the path exists."""
+        """
+        Return true if the path exists.
+
+        :param path: Path to the file
+        :type path: str
+        :return: Whether the file path exists or not
+        :rtype: bool
+        """
         return bool(self.filesize(path))
 
     def __str__(self):
-        """Return endpoint/bucket."""
+        """
+        Return endpoint/bucket concatenation.
+
+        :return: Endpoint plus bucket concatenation
+        :rtype: str
+        """
         return self.endpoint + '/' + self.bucket
